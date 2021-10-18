4336cb5b86063835d85a6a879259f39eed51f972
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2021-10-18 14:34:08 +0200

    FEAT: styled login page somewhat with material-tailwind #15

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import './styles/global.css'
 import App from './App'
 import { Layout } from './components/Layout'
+import "@material-tailwind/react/tailwind.css";
 
 render(
   <Layout>
