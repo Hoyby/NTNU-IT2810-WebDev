14ccabd3645d378e508bf50fd8deb4d2e43243dc
Hoeyby <alex.hoeyby@gmail.com>
2021-10-21 15:26:39 +0200

    FEAT: Established backend-db connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,24 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import { MovieModule } from './movie/movie.module';
-import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
+// import { MovieModule } from './movie/movie.module';
+// import { Movie } from './movie/movie.schema';
+import { RecipesModule } from './recipes/recipes.module';
+
 
 @Module({
   imports: [
+    RecipesModule,
+    MongooseModule.forRoot(
+    `mongodb://mongodb:27017/DB`
+    ,),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
       playground: true,
       debug: false,
     }),
-    ConfigModule,
-    MongooseModule.forRoot('mongodb://mongodb:27017/movieDB'),
-    MovieModule,
   ],
 })
-export class AppModule { }
+export class AppModule {}
