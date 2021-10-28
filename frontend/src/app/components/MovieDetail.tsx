c865c6c8d89034b35bb31ef95cd3fc89eb6ce436
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2021-10-28 18:57:26 +0200

    DRAFT: Movie is now fetched by id #21

--- frontend/src/app/components/MovieDetail.tsx
+++ frontend/src/app/components/MovieDetail.tsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {useParams} from "react-router-dom";
-// import {GET_MOVIE_BY_ID} from "../services/movieService/queries";
+import { MovieService } from "../services/movieService";
+import {FindMovie_findMovie} from "../services/movieService/__generated__/FindMovie";
 
 export default function MovieDetail() {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     const { id } = useParams();
+    const movieService = new MovieService()
+    const [movie, setMovie] = useState<FindMovie_findMovie>();
+    let queryResult: React.SetStateAction<FindMovie_findMovie | undefined> | null = null
+
+    async function fetchMovie() {
+            queryResult = await movieService.findMovie(id).catch((err: Error) => {
+            console.error(err.message)
+            throw err
+        })
+        console.warn(queryResult)
+        if (queryResult) setMovie(queryResult)
+    }
+
+
+
+    useEffect(() => {
+        return () => {
+            fetchMovie().catch((err: Error) => {
+                console.error(err.message)
+                throw err
+            })
+        };
+    }, );
 
     return (
         <>
             <h1>{id}</h1>
+            <h1>{movie ? movie.title : 'No movie'}</h1>
         </>
     )
 }
