37a1e1a1daa96fd58d9a8ff3ae9a234431ba0eeb
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2021-10-28 22:46:09 +0200

    FIX: fix unwanted re-renders of MovieDetail.tsx #21

--- frontend/src/app/components/MovieDetail.tsx
+++ frontend/src/app/components/MovieDetail.tsx
@@ -37,7 +37,7 @@ export default function MovieDetail() {
             console.error(err.message)
             throw err
         })
-    }, );
+    }, []);
 
     useEffect(() => {
         setShowModal(true)
