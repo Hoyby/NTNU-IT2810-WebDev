ad9b66ae59c00188bcb1d084ccc879f7064ecb92
Hoeyby <alex.hoeyby@gmail.com>
2021-10-30 17:21:23 +0200

    CHANGE: revised redux

--- frontend/src/app/pages/MoviePage/types.ts
+++
@@ -1,5 +0,0 @@
-import { GetMovies } from '../../services/movieService/__generated__/GetMovies'
-
-export interface IMoviePageState {
-    moviePage: GetMovies['getmovies']
-}
---
+++ frontend/src/app/slices/searchPageSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { SearchAndSortMovies } from '../services/movieService/__generated__/SearchAndSortMovies'
+
+export interface ISearchResultState {
+    searchPage: SearchAndSortMovies['searchandSortMovie'] // Array of movies
+}
+
+const initialState: ISearchResultState = {
+    searchPage: [], // Initialize with 0 movies
+}
+
+const SearchPageSlice = createSlice({
+    name: 'searchPage',
+    initialState,
+    reducers: {
+        setSearchPage(
+            state,
+            action: PayloadAction<SearchAndSortMovies['searchandSortMovie']>,
+        ) {
+            if (action) state.searchPage = action.payload // Set new state with the value of action
+        },
+    },
+})
+
+export const { setSearchPage: setSearchPage } = SearchPageSlice.actions
+export default SearchPageSlice.reducer
